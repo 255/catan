3cb2d6d46e6b750b2959a47e75d0f533db1f0e83
Wyatt Hepler <wyatt.hepler+git@gmail.com>
2014-09-22 12:57:46 -0600

    Parameter change in IHttpCommunicator

--- java/src/client/network/IHttpCommunicator.java
+++ java/src/client/network/IHttpCommunicator.java
@@ -14,17 +14,17 @@ public interface IHttpCommunicator {
      *
      * @throws NetworkException indicates that an error occurred while connecting to the server
      */
-    Object get(String url) throws NetworkException;
+    String get(String url) throws NetworkException;
 
     /**
      * The Http Post Methods. It sends data to a server and performs
      * a command.
      *
      * @param url the name mapped to the corresponding handler in the server on the other side
-     * @param obj object that holds the information to perform the command
+     * @param data the information to perform the command
      * @return JSON string as server response
      *
      * @throws NetworkException indicates that an error occurred while connecting to the server
      */
-    Object post(String url, Object obj) throws NetworkException;
+    String post(String url, String data) throws NetworkException;
 }
