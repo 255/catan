efd1bf63dff062dadc529eb470e195ea598df7a3
link834 <link834@gmail.com>
2014-09-29 17:02:07 -0600

    Added ModelFacade class

---
+++ java/src/shared/model/ModelFacade.java
@@ -0,0 +1,247 @@
+package shared.model;
+
+import client.communication.LogEntry;
+import client.data.PlayerInfo;
+import shared.definitions.PortType;
+import shared.locations.EdgeLocation;
+import shared.locations.HexLocation;
+import shared.locations.VertexLocation;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ *
+ */
+public class ModelFacade implements IModelFacade {
+
+    private IGame m_theGame;
+
+    // take a look at the REST api for things that the facade should do
+    // check for the pre conditions
+
+
+    /**
+     * Creates a mediator between the GUI and the Game object
+     *
+     * @param theGame the game object of the current game
+     */
+    ModelFacade(IGame theGame) {
+        m_theGame = theGame;
+    }
+
+    /**
+     * Takes an edge location and determines if a road can be placed on it
+     *
+     * @param edge the location of the side of a terrain hex
+     * @return a boolean value that reports if the user can place a road
+     */
+    @Override
+    public boolean canPlaceRoad(EdgeLocation edge) {
+        return false;
+    }
+
+    /**
+     * Takes a vertex location and determines if a settlement can be placed on it
+     *
+     * @param vertex the location of a corner/intersection of terrain hexes
+     * @return a boolean value that reports if the user can place a settlement
+     */
+    @Override
+    public boolean canPlaceSettlement(VertexLocation vertex) {
+        return false;
+    }
+
+    /**
+     * Takes a vertex location and determines if a city can be placed on it
+     *
+     * @param vertex the location of a corner/intersection of terrain hexes
+     * @return a boolean value that reports if the user can place a city
+     */
+    @Override
+    public boolean canPlaceCity(VertexLocation vertex) {
+        return false;
+    }
+
+    /**
+     * Takes an edge location and places a road on it
+     *
+     * @param edge the location of the side of a terrain hex
+     */
+    @Override
+    public void placeRoad(EdgeLocation edge) {
+
+    }
+
+    /**
+     * Takes a vertex location and places a settlement on it
+     *
+     * @param vertex the location of an intersection of terrain hexes
+     */
+    @Override
+    public void placeSettlement(VertexLocation vertex) {
+
+    }
+
+    /**
+     * Takes a vertex location and places a city on it
+     *
+     * @param vertex the location of an intersection of terrain hexes
+     */
+    @Override
+    public void placeCity(VertexLocation vertex) {
+
+    }
+
+    /**
+     * Takes a hex location and places the robber on it
+     *
+     * @param hex the location of a terrain hex
+     */
+    @Override
+    public void placeRobber(HexLocation hex) {
+
+    }
+
+    /**
+     * Takes an IPlayer object and removes the given card object from the player
+     *
+     * @param player   the player to take cards from
+     * @param cardType the Card object type to decrement from the player hand
+     */
+    @Override
+    public void takeCardFromPlayer(IPlayer player, Object cardType) {
+
+    }
+
+    /**
+     * Takes an IPlayer object and gives the given card object to the player
+     *
+     * @param player   the player to take cards from
+     * @param cardType the Card object type to decrement from the player hand
+     */
+    @Override
+    public void giveCardToPlayer(IPlayer player, Object cardType) {
+
+    }
+
+    /**
+     * Returns the chat history of the game
+     *
+     * @return the log initialized with all the chat messages
+     */
+    @Override
+    public List<LogEntry> getChatHistory() {
+        return null;
+    }
+
+    /**
+     * Returns the move/action history of the game
+     *
+     * @return the log initialized with all the move/action messages
+     */
+    @Override
+    public List<LogEntry> getMoveHistory() {
+        return null;
+    }
+
+    /**
+     * Returns the info for the current player
+     *
+     * @return the PlayerInfo object initialized with all the player card/piece counts
+     */
+    @Override
+    public PlayerInfo getCurPlayerInfo() {
+        return null;
+    }
+
+    /**
+     * Returns the info for the current player's placed roads
+     *
+     * @return the Collection of IRoad objects initialized with all the locations of the current player's roads
+     */
+    @Override
+    public Collection<IRoad> getRoads() {
+        return null;
+    }
+
+    /**
+     * Returns the info for the current player's placed settlements
+     *
+     * @return the Collection of ITown objects initialized with all the locations of the placed settlements
+     */
+    @Override
+    public Collection<ITown> getSettlements() {
+        return null;
+    }
+
+    /**
+     * Returns the info for the current player's placed cities
+     *
+     * @return the Collection of ITown objects initialized with all the locations of the placed cities
+     */
+    @Override
+    public Collection<ITown> getCities() {
+        return null;
+    }
+
+    /**
+     * Returns the info for the number pieces
+     *
+     * @return the Collection of ITile terrain hex objects initialized with all the locations of the number pieces
+     */
+    @Override
+    public Collection<ITile> getNumberPieces() {
+        return null;
+    }
+
+    /**
+     * Returns the info for the terrain hexes
+     *
+     * @return the Collection of ITile objects initialized with all the locations and types of terrain hexes
+     */
+    @Override
+    public Collection<ITile> getHexes() {
+        return null;
+    }
+
+    /**
+     * Returns the terrain hex location for the current location of the robber
+     *
+     * @return the hex location of where the robber is currently placed
+     */
+    @Override
+    public HexLocation getRobber() {
+        return null;
+    }
+
+    /**
+     * Returns the info for the current player's resource counts
+     *
+     * @return the ResourceBundle object containing the counts for the current player
+     */
+    @Override
+    public IResourceBundle getPlayerResources() {
+        return null;
+    }
+
+    /**
+     * Returns the ports the current player has
+     *
+     * @return the ports that the current player has
+     */
+    @Override
+    public Collection<PortType> getPlayerPorts() {
+        return null;
+    }
+
+    /**
+     * Takes in a chat message to be added to the chat log
+     *
+     * @param message the string of text to add to the chat log
+     */
+    @Override
+    public void sendChat(String message) {
+
+    }
+}
