abb76e9b9464500fc0740a45262b935d7797dcc1
Wyatt Hepler <wyatt.hepler+git@gmail.com>
2014-09-29 16:53:34 -0600

    Added Prices

---
+++ java/src/shared/model/Prices.java
@@ -0,0 +1,13 @@
+package shared.model;
+
+/**
+ * The prices of the objects in the game.
+ *
+ * @author Wyatt
+ */
+public final class Prices {
+    public static final ResourceBundle ROAD       = new ResourceBundle(1, 1, 0, 0, 0);
+    public static final ResourceBundle SETTLEMENT = new ResourceBundle(1, 1, 1, 1, 0);
+    public static final ResourceBundle CITY       = new ResourceBundle(0, 0, 0, 2, 3);
+    public static final ResourceBundle DEV_CARD   = new ResourceBundle(0, 0, 1, 1, 1);
+}
