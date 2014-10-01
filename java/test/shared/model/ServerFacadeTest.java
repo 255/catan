874a728a4a18b090711945a7335f02436254b650
jeffreykbacon <jeffreykbacon@gmail.com>
2014-09-30 20:30:29 -0600

    adding facade tests

--- java/test/shared/model/ServerFacadeTest.java
+++ java/test/shared/model/ServerFacadeTest.java
@@ -1,21 +1,66 @@
 package shared.model;
 
-import static org.junit.Assert.*;
+import org.junit.*;
+import org.junit.Assert.*;
+import shared.model.IGame;
+import shared.model.IModelSerializer;
+import shared.model.ModelSerializer;
+import shared.model.ServerFacade;
+import client.network.IServerProxy;
+import client.network.TestServerProxy;
+
+import java.lang.Exception;
+import java.lang.System;
 
 public class ServerFacadeTest {
 
-    @org.junit.Test
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        return;
+    }
+
+    private IServerFacade facade;
+    private IServerProxy proxy;
+    private IModelSerializer serializer;
+    private IGame game;
+
+    @Before
+    public void setUp() throws Exception {
+        facade = new ServerFacade();
+        proxy = new TestServerProxy();
+        serializer = new ModelSerializer();
+        game = serializer.convertJSONtoModel(proxy.getGameState());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
     public void testGetChatHistory() throws Exception {
-
+//        assertEquals(facade.getChatHistory(), game.getChatHistory().getMessages());
+//        System.out.println(game.getChatHistory().getMessages().get(0).getMessage());
     }
 
-    @org.junit.Test
+    @Test
     public void testGetMoveHistory() throws Exception {
-
+//        assertEquals(facade.getMoveHistory(), game.getGameplayLog());
+//        for (int i = 0; i < facade.getMoveHistory().size(); i++) {
+//            assertEquals(facade.getMoveHistory().get(i), game.getGameplayLog().get(i));
+//        }
+//        System.out.println(game.getGameplayLog().getMessages().get(0).getMessage());
     }
 
-    @org.junit.Test
+    @Test
     public void testSendChat() throws Exception {
-
+        facade.sendChat("This is a test message.");
+        System.out.println(game.getChatHistory().getMessages().get(0).getMessage());
+        assert("This is a test message.".equals(game.getChatHistory().getMessages().get(0).getMessage()));
     }
 }
