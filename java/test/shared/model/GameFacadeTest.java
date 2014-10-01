448efbf03b9bc7e06c47d35ee4f4e3dfd21dc074
jeffreykbacon <jeffreykbacon@gmail.com>
2014-09-30 18:32:02 -0600

    added facade test files

---
+++ java/test/shared/model/GameFacadeTest.java
@@ -0,0 +1,106 @@
+package shared.model;
+
+import static org.junit.Assert.*;
+
+public class GameFacadeTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testCanPlaceRoad() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testCanPlaceSettlement() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testCanPlaceCity() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testPlaceRoad() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testPlaceSettlement() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testPlaceCity() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testPlaceRobber() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testTakeCardFromPlayer() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGiveCardToPlayer() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetCurPlayerInfo() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetRoads() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetSettlements() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetCities() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetNumberPieces() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetHexes() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetRobber() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetPlayerResources() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetPlayerPorts() throws Exception {
+
+    }
+}
---
+++ java/test/shared/model/ServerFacadeTest.java
@@ -0,0 +1,21 @@
+package shared.model;
+
+import static org.junit.Assert.*;
+
+public class ServerFacadeTest {
+
+    @org.junit.Test
+    public void testGetChatHistory() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetMoveHistory() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSendChat() throws Exception {
+
+    }
+}
