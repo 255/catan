07b61b04867391d1da112c8d3e914b754e624952
sdaltonb <sdaltonb@stevens-mbp.home>
2014-10-01 21:37:56 -0600

    Added ModelInitializer tests

--- java/test/shared/model/ModelInitializerTest.java
+++ java/test/shared/model/ModelInitializerTest.java
@@ -3,6 +3,13 @@ package shared.model;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import shared.definitions.CatanColor;
+import shared.definitions.DevCardType;
+import shared.definitions.ResourceType;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -400,7 +407,7 @@ public class ModelInitializerTest {
                         "\"playedDevCard\":" + "false," +
                         "\"discarded\":" + "false," +
                         "\"playerID\":" + "11," +
-                        "\"playerIndex\":" + "2," +
+                        "\"playerIndex\":" + "3," +
                         "\"name\":" + "\"Mark\"," +
                         "\"color\":" + "\"green\"" +
                         "}," + "]," +
@@ -437,6 +444,79 @@ public class ModelInitializerTest {
 
     @Test
     public void testConvertJSONtoModel() throws Exception {
+        // Test that the Development Card deck is correctly deserialized
+        IDevCardHand testDevCardHand = m_game.getDevCards();
+        assertEquals("Incorrect number of Monopoly cards", 2, testDevCardHand.getCount(DevCardType.MONOPOLY));
+        assertEquals("Incorrect number of Soldier cards", 14, testDevCardHand.getCount(DevCardType.SOLDIER));
+        assertEquals("Incorrect number of Year of Plenty cards", 2, testDevCardHand.getCount(DevCardType.YEAR_OF_PLENTY));
+        assertEquals("Incorrect number of Road Building cards", 2, testDevCardHand.getCount(DevCardType.ROAD_BUILD));
+        assertEquals("Incorrect number of Monument cards", 5, testDevCardHand.getCount(DevCardType.MONUMENT));
+
+        List<IPlayer> playerList = m_game.getPlayers();
+
+        IPlayer testPlayer = playerList.get(0);
+        IResourceBank testPlayerResourceCards = testPlayer.getResources();
+        assertEquals("Incorrect number of Brick cards in player hand", 0, testPlayerResourceCards.getCount(ResourceType.BRICK));
+        assertEquals("Incorrect number of Wood cards in player hand", 0, testPlayerResourceCards.getCount(ResourceType.WOOD));
+        assertEquals("Incorrect number of Sheep cards in player hand", 0, testPlayerResourceCards.getCount(ResourceType.SHEEP));
+        assertEquals("Incorrect number of Wheat cards in player hand", 0, testPlayerResourceCards.getCount(ResourceType.WHEAT));
+        assertEquals("Incorrect number of Ore cards in player hand", 0, testPlayerResourceCards.getCount(ResourceType.ORE));
+
+        IDevCardHand testPlayerOldDevCardHand = testPlayer.getPlayableDevCards();
+        assertEquals("Incorrect number of Monopoly cards in player hand", 0, testPlayerOldDevCardHand.getCount(DevCardType.MONOPOLY));
+        assertEquals("Incorrect number of Soldier cards in player hand", 0, testPlayerOldDevCardHand.getCount(DevCardType.SOLDIER));
+        assertEquals("Incorrect number of Year of Plenty cards in player hand", 0, testPlayerOldDevCardHand.getCount(DevCardType.YEAR_OF_PLENTY));
+        assertEquals("Incorrect number of Road Building cards in player hand", 0, testPlayerOldDevCardHand.getCount(DevCardType.ROAD_BUILD));
+        assertEquals("Incorrect number of Monument cards in player hand", 0, testPlayerOldDevCardHand.getCount(DevCardType.MONUMENT));
+
+        IDevCardHand testPlayerNewDevCardHand = testPlayer.getNewDevCards();
+        assertEquals("Incorrect number of Monopoly cards in player hand", 0, testPlayerNewDevCardHand.getCount(DevCardType.MONOPOLY));
+        assertEquals("Incorrect number of Soldier cards in player hand", 0, testPlayerNewDevCardHand.getCount(DevCardType.SOLDIER));
+        assertEquals("Incorrect number of Year of Plenty cards in player hand", 0, testPlayerNewDevCardHand.getCount(DevCardType.YEAR_OF_PLENTY));
+        assertEquals("Incorrect number of Road Building cards in player hand", 0, testPlayerNewDevCardHand.getCount(DevCardType.ROAD_BUILD));
+        assertEquals("Incorrect number of Monument cards in player hand", 0, testPlayerNewDevCardHand.getCount(DevCardType.MONUMENT));
+
+        IPieceBank playerPieceBank = testPlayer.getPieceBank();
+        assertEquals("Incorrect number of roads in player's bank", 15, playerPieceBank.availableRoads());
+        assertEquals("Incorrect number of cities in player's bank", 4, playerPieceBank.availableCities());
+        assertEquals("Incorrect number of settlements in player's bank", 5, playerPieceBank.availableSettlements());
+
+        assertEquals("Incorrect number of soldiers player has played", 0, testPlayer.getSoldiers());
+        assertEquals("Incorrect number of monuments player has played", 0, testPlayer.getMonuments());
+        assertEquals("Incorrect number of victory points player", 0, testPlayer.getVictoryPoints());
+
+        assertEquals("Incorrect player id", 0, testPlayer.getId());
+        assertEquals("Incorrect player index", 0, testPlayer.getIndex());
+        assertEquals("Incorrect player name", "Sam", testPlayer.getName());
+        assertEquals("Incorrect player color", CatanColor.ORANGE, testPlayer.getColor());
+
+        testPlayer = playerList.get(1);
+        assertEquals("Incorrect player id", 1, testPlayer.getId());
+        assertEquals("Incorrect player index", 1, testPlayer.getIndex());
+        assertEquals("Incorrect player name", "Brooke", testPlayer.getName());
+        assertEquals("Incorrect player color", CatanColor.BLUE, testPlayer.getColor());
+
+        testPlayer = playerList.get(2);
+        assertEquals("Incorrect player id", 10, testPlayer.getId());
+        assertEquals("Incorrect player index", 2, testPlayer.getIndex());
+        assertEquals("Incorrect player name", "Pete", testPlayer.getName());
+        assertEquals("Incorrect player color", CatanColor.RED, testPlayer.getColor());
+
+        testPlayer = playerList.get(3);
+        assertEquals("Incorrect player id", 3, testPlayer.getId());
+        assertEquals("Incorrect player index", 11, testPlayer.getIndex());
+        assertEquals("Incorrect player name", "Mark", testPlayer.getName());
+        assertEquals("Incorrect player color", CatanColor.GREEN, testPlayer.getColor());
+
+ //Robber. Ports. Map
+
+        // Test that the Resource Bank is correctly deserialized
+        IResourceBank testResourceBank = m_game.getResourceBank();
+        assertEquals("Incorrect number of Brick cards", 24, testResourceBank.getCount(ResourceType.BRICK));
+        assertEquals("Incorrect number of Wood cards", 24, testResourceBank.getCount(ResourceType.WOOD));
+        assertEquals("Incorrect number of Sheep cards", 24, testResourceBank.getCount(ResourceType.SHEEP));
+        assertEquals("Incorrect number of Wheat cards", 24, testResourceBank.getCount(ResourceType.WHEAT));
+        assertEquals("Incorrect number of Ore cards", 24, testResourceBank.getCount(ResourceType.ORE));
 
     }
 }
