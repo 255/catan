c002c813831521b5d098935c228782c3b4a5c6fb
link834 <link834@gmail.com>
2014-11-11 17:28:11 -0700

    started MaritimeTradeCommand

--- java/src/server/command/MaritimeTradeCommand.java
+++ java/src/server/command/MaritimeTradeCommand.java
@@ -1,11 +1,33 @@
 package server.command;
 
+import shared.definitions.ResourceType;
+import shared.model.Game;
+
 /**
  * Class that represents the MaritimeTrade request
  *
  * @author StevenBarnett
  */
 public class MaritimeTradeCommand implements ICommand {
+
+    private int playerIndex;
+    private int ratio;
+    private ResourceType inputResource;
+    private ResourceType outputResource;
+    private Game theGame;
+
+    MaritimeTradeCommand(
+            int playerIndex,
+            int ratio,
+            ResourceType inputResource,
+            ResourceType outputResource,
+            Game theGame) {
+        this.playerIndex = playerIndex;
+        this.ratio = ratio;
+        this.inputResource = inputResource;
+        this.outputResource = outputResource;
+        this.theGame = theGame;
+    }
 
     /**
      * Makes a maritime trade request. Resources that the
@@ -14,5 +36,9 @@ public class MaritimeTradeCommand implements ICommand {
      */
     public void execute() {
 
+
+        // remove the input resources from the player
+
+        // add the output resources to the player
     }
 }
