065b8917251b4413e5e1ca3c2705c5a34c013d4d
sdaltonb <sdaltonb@stevens-mbp-4.app.byu.edu>
2014-11-11 17:29:33 -0700

    Added a randomDevCardType() method to DevCardType

--- java/src/shared/definitions/DevCardType.java
+++ java/src/shared/definitions/DevCardType.java
@@ -1,8 +1,23 @@
 package shared.definitions;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 public enum DevCardType
 {
-	
-	SOLDIER, YEAR_OF_PLENTY, MONOPOLY, ROAD_BUILD, MONUMENT, NUMBER_OF_TYPES
+	SOLDIER, YEAR_OF_PLENTY, MONOPOLY, ROAD_BUILD, MONUMENT;
+
+
+    private static final List<DevCardType> VALUES =
+            Collections.unmodifiableList(Arrays.asList(values()));
+    private static final int SIZE = VALUES.size();
+    private static final Random RANDOM = new Random();
+
+    public static DevCardType randomDevCardType()  {
+        return VALUES.get(RANDOM.nextInt(SIZE));
+    }
+
 }
 
