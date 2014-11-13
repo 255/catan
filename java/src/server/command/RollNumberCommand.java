2212503e4b01c39691d85fa5ffae92ecc34bfeca
jeffreykbacon <jeffreykbacon@gmail.com>
2014-11-12 17:00:48 -0700

    work so far on RollNumberCommand

--- java/src/server/command/RollNumberCommand.java
+++ java/src/server/command/RollNumberCommand.java
@@ -1,11 +1,30 @@
 package server.command;
 
+import shared.model.IGame;
+import shared.model.IPlayer;
+
 /**
  * Class that represents the RollNumber request
  *
  * @author StevenBarnett
  */
 public class RollNumberCommand extends AbstractCommand {
+    private int m_playerIndex;
+    private int m_number;
+
+    public RollNumberCommand(IGame game, int playerIndex, int number) throws IllegalCommandException{
+        super(game);
+        m_playerIndex = playerIndex;
+        m_number = number;
+
+        IPlayer player = getGame().getPlayers().get(m_playerIndex);
+
+        if(!getGame().isPlayersTurn(player)) {
+            throw new IllegalCommandException(
+                    "It is not this player's turn: " + m_playerIndex
+            );
+        }
+    }
 
     /**
      * Determines what resources need to be given to
