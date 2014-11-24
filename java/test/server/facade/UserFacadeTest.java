cc7747f0f9c62073d246eed1e48d8d0d1d5c0a3b
link834 <link834@gmail.com>
2014-11-24 00:45:56 -0700

    created a file for UserFacade tests

---
+++ java/test/server/facade/UserFacadeTest.java
@@ -0,0 +1,30 @@
+package server.facade;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Spencer Weight - 11/23/2014.
+ */
+public class UserFacadeTest {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Before
+    public void tearDown() {
+
+    }
+
+    @Test
+    public void testRegister() {
+
+    }
+
+    @Test
+    public void testLogin() {
+        //
+    }
+}
