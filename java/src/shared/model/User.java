9734598910af0815754db6df293eefcc67e08cb9
Wyatt Hepler <wyatt.hepler+git@gmail.com>
2014-11-24 19:59:57 -0700

    UserManager tests

--- java/src/shared/model/User.java
+++ java/src/shared/model/User.java
@@ -28,4 +28,26 @@ public class User implements IUser {
     public int getId() {
         return playerID;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (playerID != user.playerID) return false;
+        if (!m_password.equals(user.m_password)) return false;
+        if (!name.equals(user.name)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + m_password.hashCode();
+        result = 31 * result + playerID;
+        return result;
+    }
 }
