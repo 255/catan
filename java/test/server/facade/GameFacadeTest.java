8201b2dbac6c477fe1874c1c1987dc99db1902bf
Wyatt Hepler <1.61803@byu.net>
2014-11-25 15:53:53 -0700

    Made GameFacadeTest platform-independent

--- java/test/server/facade/GameFacadeTest.java
+++ java/test/server/facade/GameFacadeTest.java
@@ -17,6 +17,9 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 
+import java.util.Map;
+import java.util.HashMap;
+
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -42,6 +45,7 @@ public class GameFacadeTest {
     @Test
     public void modelTest() throws Exception {
         String expected = "{\"deck\":{\"monopoly\":2,\"monument\":5,\"roadBuilding\":2,\"soldier\":14,\"yearOfPlenty\":2},\"map\":{\"hexes\":[{\"resource\":\"brick\",\"number\":5,\"location\":{\"x\":1,\"y\":0}},{\"resource\":\"wood\",\"number\":3,\"location\":{\"x\":0,\"y\":-1}},{\"resource\":\"sheep\",\"number\":10,\"location\":{\"x\":1,\"y\":1}},{\"resource\":\"brick\",\"number\":8,\"location\":{\"x\":-1,\"y\":-1}},{\"resource\":\"wheat\",\"number\":11,\"location\":{\"x\":0,\"y\":0}},{\"resource\":\"sheep\",\"number\":10,\"location\":{\"x\":-1,\"y\":0}},{\"resource\":\"wood\",\"number\":4,\"location\":{\"x\":0,\"y\":1}},{\"resource\":\"ore\",\"number\":5,\"location\":{\"x\":-2,\"y\":0}},{\"resource\":\"wheat\",\"number\":8,\"location\":{\"x\":0,\"y\":2}},{\"resource\":\"sheep\",\"number\":9,\"location\":{\"x\":-1,\"y\":1}},{\"resource\":\"wheat\",\"number\":2,\"location\":{\"x\":-2,\"y\":1}},{\"resource\":\"ore\",\"number\":3,\"location\":{\"x\":-1,\"y\":2}},{\"resource\":\"wood\",\"number\":6,\"location\":{\"x\":-2,\"y\":2}},{\"resource\":\"wood\",\"number\":11,\"location\":{\"x\":2,\"y\":-2}},{\"resource\":\"sheep\",\"number\":12,\"location\":{\"x\":2,\"y\":-1}},{\"resource\":\"brick\",\"number\":4,\"location\":{\"x\":1,\"y\":-2}},{\"resource\":\"wheat\",\"number\":9,\"location\":{\"x\":2,\"y\":0}},{\"location\":{\"x\":0,\"y\":-2}},{\"resource\":\"ore\",\"number\":6,\"location\":{\"x\":1,\"y\":-1}}],\"roads\":[],\"cities\":[],\"settlements\":[],\"radius\":3,\"ports\":[{\"ratio\":3,\"direction\":\"SE\",\"location\":{\"x\":-3,\"y\":0}},{\"ratio\":2,\"resource\":\"wheat\",\"direction\":\"S\",\"location\":{\"x\":-1,\"y\":-2}},{\"ratio\":2,\"resource\":\"wood\",\"direction\":\"NE\",\"location\":{\"x\":-3,\"y\":2}},{\"ratio\":2,\"resource\":\"brick\",\"direction\":\"NE\",\"location\":{\"x\":-2,\"y\":3}},{\"ratio\":2,\"resource\":\"ore\",\"direction\":\"S\",\"location\":{\"x\":1,\"y\":-3}},{\"ratio\":3,\"direction\":\"SW\",\"location\":{\"x\":3,\"y\":-3}},{\"ratio\":3,\"direction\":\"NW\",\"location\":{\"x\":2,\"y\":1}},{\"ratio\":3,\"direction\":\"N\",\"location\":{\"x\":0,\"y\":3}},{\"ratio\":2,\"resource\":\"sheep\",\"direction\":\"NW\",\"location\":{\"x\":3,\"y\":-1}}],\"robber\":{\"x\":0,\"y\":-2}},\"players\":[{\"resources\":{\"wood\":0,\"brick\":0,\"sheep\":0,\"wheat\":0,\"ore\":0},\"oldDevCards\":{\"monopoly\":0,\"monument\":0,\"roadBuilding\":0,\"soldier\":0,\"yearOfPlenty\":0},\"newDevCards\":{\"monopoly\":0,\"monument\":0,\"roadBuilding\":0,\"soldier\":0,\"yearOfPlenty\":0},\"roads\":15,\"cities\":4,\"settlements\":5,\"soldiers\":0,\"victoryPoints\":0,\"monuments\":0,\"playedDevCard\":false,\"discarded\":false,\"playerID\":0,\"playerIndex\":0,\"name\":\"Sam\",\"color\":\"red\"},{\"resources\":{\"wood\":0,\"brick\":0,\"sheep\":0,\"wheat\":0,\"ore\":0},\"oldDevCards\":{\"monopoly\":0,\"monument\":0,\"roadBuilding\":0,\"soldier\":0,\"yearOfPlenty\":0},\"newDevCards\":{\"monopoly\":0,\"monument\":0,\"roadBuilding\":0,\"soldier\":0,\"yearOfPlenty\":0},\"roads\":15,\"cities\":4,\"settlements\":5,\"soldiers\":0,\"victoryPoints\":0,\"monuments\":0,\"playedDevCard\":false,\"discarded\":false,\"playerID\":1,\"playerIndex\":1,\"name\":\"Pete\",\"color\":\"blue\"},{\"resources\":{\"wood\":0,\"brick\":0,\"sheep\":0,\"wheat\":0,\"ore\":0},\"oldDevCards\":{\"monopoly\":0,\"monument\":0,\"roadBuilding\":0,\"soldier\":0,\"yearOfPlenty\":0},\"newDevCards\":{\"monopoly\":0,\"monument\":0,\"roadBuilding\":0,\"soldier\":0,\"yearOfPlenty\":0},\"roads\":15,\"cities\":4,\"settlements\":5,\"soldiers\":0,\"victoryPoints\":0,\"monuments\":0,\"playedDevCard\":false,\"discarded\":false,\"playerID\":2,\"playerIndex\":2,\"name\":\"Mark\",\"color\":\"green\"},{\"resources\":{\"wood\":0,\"brick\":0,\"sheep\":0,\"wheat\":0,\"ore\":0},\"oldDevCards\":{\"monopoly\":0,\"monument\":0,\"roadBuilding\":0,\"soldier\":0,\"yearOfPlenty\":0},\"newDevCards\":{\"monopoly\":0,\"monument\":0,\"roadBuilding\":0,\"soldier\":0,\"yearOfPlenty\":0},\"roads\":15,\"cities\":4,\"settlements\":5,\"soldiers\":0,\"victoryPoints\":0,\"monuments\":0,\"playedDevCard\":false,\"discarded\":false,\"playerID\":3,\"playerIndex\":3,\"name\":\"Bob\",\"color\":\"puce\"}],\"log\":{\"lines\":[]},\"chat\":{\"lines\":[]},\"bank\":{\"wood\":19,\"brick\":19,\"sheep\":19,\"wheat\":19,\"ore\":19},\"turnTracker\":{\"status\":\"FirstRound\",\"currentTurn\":0,\"longestRoad\":-1,\"largestArmy\":-1},\"winner\":-1,\"version\":0}";
+
         URL oracle;
         URLConnection yc;
         BufferedReader in;
@@ -71,6 +75,27 @@ public class GameFacadeTest {
             System.out.println(e.getMessage());
         }
 
-        assertEquals("Result did not equal expected, could be because we changed our default game", expected, result);
+        assertEquals("Result did not equal expected, could be because we changed our default game", countChars(expected), countChars(result));
     }
+
+  /** Count the characters of a string and return the counts in a map.
+   * Due to weirdness with Java returning ports in different orders (depending debug/release mode, etc.)
+  *  we count the characters in the expected string.
+  * If characters occur in the same frequency, the strings almost certainly are equal, albeit potentially in a different order
+  */
+  private Map<Character, Integer> countChars(String str) {
+      Map<Character, Integer> expectedCharCount = new HashMap<Character, Integer>();
+
+      for (char c : str.toCharArray()) {
+        if (expectedCharCount.containsKey(c)) {
+          expectedCharCount.put(c, expectedCharCount.get(c) + 1);
+        }
+        else {
+          expectedCharCount.put(c, 1);
+        }
+      }
+
+      return expectedCharCount;
+  }
 }
+
