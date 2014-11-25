ce2dc2077a4642bbb228705879126916272286c0
sdaltonb <sdaltonb@Stevens-MacBook-Pro-6.local>
2014-11-25 09:36:15 -0700

    Implemented MaritimeTradeCommand tests

--- java/test/server/command/MaritimeTradeCommandTest.java
+++ java/test/server/command/MaritimeTradeCommandTest.java
@@ -2,36 +2,120 @@ package server.command;
 
 import org.junit.After;
 import org.junit.Before;
-import shared.model.GameModelFacade;
-import shared.model.IGame;
-import shared.model.ModelInitializer;
+import org.junit.Test;
+import shared.definitions.ResourceType;
+import shared.model.*;
 
 import static org.junit.Assert.*;
 
 public class MaritimeTradeCommandTest {
 
+    IGame game;
+
     @Before
     public void setUp() throws Exception {
+
+        game = initAGame("sample/maritime_trade_test.json");
+    }
+
+    @Test
+    public void testPreConditions() throws Exception {
+
+        // Test trying to trade when it is not your turn.
+        game.setGameState(GameState.Playing);
+        boolean isExceptionThrown = false;
+        try {
+            new MaritimeTradeCommand(game, game.getPlayer(0), ResourceType.BRICK, ResourceType.ORE, 4);
+        } catch (IllegalCommandException ex) {
+            isExceptionThrown = true;
+        }
+        assertTrue(isExceptionThrown);
+
+        isExceptionThrown = false;
+        try {
+            new MaritimeTradeCommand(game, game.getPlayer(2), ResourceType.BRICK, ResourceType.ORE, 4).execute();
+        } catch (IllegalCommandException ex) {
+            isExceptionThrown = true;
+        }
+        assertFalse(isExceptionThrown);
+
+        // Test 4:1 trade when you don't have enough resources to do it.
+        isExceptionThrown = false;
+        try {
+            new MaritimeTradeCommand(game, game.getPlayer(2), ResourceType.BRICK, ResourceType.ORE, 4);
+        } catch (IllegalCommandException ex) {
+            isExceptionThrown = true;
+        }
+        assertTrue(isExceptionThrown);
+
+        // Test trading 4:1 of the same resource.
+        isExceptionThrown = false;
+        try {
+            new MaritimeTradeCommand(game, game.getPlayer(2), ResourceType.ORE, ResourceType.WOOD, 4).execute();
+        } catch (IllegalCommandException ex) {
+            isExceptionThrown = true;
+        }
+        assertFalse(isExceptionThrown);
+
+        // Test 3:1 when you don't have a 3:1 port.
+        game.setCurrentPlayer(game.getPlayer(0));
+        isExceptionThrown = false;
+        try {
+            new MaritimeTradeCommand(game, game.getPlayer(0), ResourceType.WHEAT, ResourceType.WOOD, 3);
+        } catch (IllegalCommandException ex) {
+            isExceptionThrown = true;
+        }
+        assertTrue(isExceptionThrown);
+
+        // Test 3:1 when you don't have enough resources.
+        game.setCurrentPlayer(game.getPlayer(3));
+        isExceptionThrown = false;
+        try {
+            new MaritimeTradeCommand(game, game.getPlayer(3), ResourceType.SHEEP, ResourceType.WOOD, 3);
+        } catch (IllegalCommandException ex) {
+            isExceptionThrown = true;
+        }
+        assertTrue(isExceptionThrown);
+
+        // Test 2:1 when you don't have a 2:1 port.
+        isExceptionThrown = false;
+        try {
+            new MaritimeTradeCommand(game, game.getPlayer(3), ResourceType.ORE, ResourceType.WOOD, 2);
+        } catch (IllegalCommandException ex) {
+            isExceptionThrown = true;
+        }
+        assertTrue(isExceptionThrown);
+
+        // Test 2:1 when you don't have enough resources.
+        game.setCurrentPlayer(game.getPlayer(2));
+        isExceptionThrown = false;
+        try {
+            new MaritimeTradeCommand(game, game.getPlayer(2), ResourceType.SHEEP, ResourceType.WOOD, 2);
+        } catch (IllegalCommandException ex) {
+            isExceptionThrown = true;
+        }
+        assertTrue(isExceptionThrown);
+
+    }
+
+    @Test
+    public void testMaritimeTrade() {
+
+        // Test that the correct resource have been taken and given in a 4:1
+        // Test that the correct resource have been taken and given in a 2:1
+        // Test that the correct resource have been taken and given in a 2:1
     }
 
     @After
     public void tearDown() throws Exception {
 
+        game = null;
     }
-
-    // Test trying to trade when it is not your turn.
-    // Test 4:1 trade when you don't have enough resources to do it.
-    // Test trading 4:1 of the same resource.
-    // Test 3:1 when you don't have a 3:1 port.
-    // Test 3:1 when you don't have enough resources.
-    // Test 2:1 when you don't have a 2:1 port.
-    // Test 2:1 when you don't have enough resources.
-    // Test
 
     private IGame initAGame(String jsonTestFile) throws Exception {
         String emptyBoardJSON = new String(java.nio.file.Files.readAllBytes(java.nio.file.Paths.get(jsonTestFile)));
         ModelInitializer initModel = new ModelInitializer();
-        initModel.initializeClientModel(emptyBoardJSON, 0);
+        initModel.initializeClientModel(emptyBoardJSON, 5);
         return GameModelFacade.instance().getGame();
     }
 
