cee0b26bb63754dcb3d895a93bb1e308edc56ea9
link834 <link834@gmail.com>
2014-12-06 11:19:47 -0700

    added some notes on how to implement FolderUserDAO as well as the file FolderUserDAO

--- java/src/server/persistence/AbstractFolderDAO.java
+++ java/src/server/persistence/AbstractFolderDAO.java
@@ -3,5 +3,17 @@ package server.persistence;
 /**
  * Handle interactions with the folder persistence implementation.
  */
-public abstract class AbstractFolderDAO {
+public abstract class AbstractFolderDAO<T> {
+
+    // create the logger here
+
+    // create some error handling
+
+    public void save(T toSave) {
+
+    }
+
+    public T load(String fileName) {
+        return null;
+    }
 }
