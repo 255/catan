13522c9861127383f541ed9ee1e2d41b2c278ca7
link834 <link834@gmail.com>
2014-12-09 00:55:23 -0700

    implemented the checkpoint saving in the FolderCommandsDAO

--- java/src/server/plugin/FolderCommandsDAO.java
+++ java/src/server/plugin/FolderCommandsDAO.java
@@ -6,13 +6,10 @@ import server.persistence.ICommandsDAO;
 import server.persistence.PersistenceException;
 import shared.model.IGame;
 import shared.model.ModelException;
-
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
@@ -30,12 +27,7 @@ public class FolderCommandsDAO extends AbstractFolderDAO implements ICommandsDAO
         String gameFolder = Integer.toString(command.getGame().getID());
         AbstractPersistenceManager.ensureDirectoryExists(getDirectory().resolve(gameFolder));
         writeFile(command, gameFolder, Integer.toString(command.getGame().getModelVersion()));
-
-        // TODO: implement checkpoint thingy saving
-        // check the number of commands in the commands directory for the game
-        // if the number of saved commands is N
-            // save the current game to disk
-            // clear the commands folder
+        saveCheckpoint(command);
     }
 
     @Override
@@ -63,9 +55,43 @@ public class FolderCommandsDAO extends AbstractFolderDAO implements ICommandsDAO
             throw new PersistenceException("Failed executing command.", e);
         }
 
-        // TODO: implement checkpoint thingy loading
+        // TODO: implement checkpoint thingy loading - needs code review
         // after all the commands have been applied to the game
-            // save the game
-            // clear the commands folder
+        // save the game and clear the folder
+        saveGameAndClear(game);
+    }
+
+    private void saveCheckpoint(ICommand command) throws PersistenceException {
+        // TODO: implement checkpoint thingy saving - needs code review
+        // check the number of commands in the commands directory for the game
+        DirectoryStream<Path> directoryStream = null;
+        try {
+            directoryStream = Files.newDirectoryStream(getDirectory().resolve(Integer.toString(command.getGame().getID())));
+        } catch (IOException e) {
+            throw new PersistenceException("Failed to read in the commands directory", e);
+        }
+
+        // if the number of saved commands is N or greater
+        int numCommands = 0;
+        for(Path p : directoryStream) {
+            numCommands++;
+        }
+
+        // save game and clear the folder
+        if(numCommands >= getPersistenceManager().getCommandsBetweenCheckpoints()) {
+            saveGameAndClear(command);
+        }
+    }
+
+    private void saveGameAndClear(ICommand command) throws PersistenceException {
+        saveGameAndClear(command.getGame());
+    }
+
+    private void saveGameAndClear(IGame game) throws PersistenceException {
+        // save the current game to disk
+        getPersistenceManager().createGamesDAO().saveGame(game);
+
+        // clear the commands folder
+        clear();
     }
 }
